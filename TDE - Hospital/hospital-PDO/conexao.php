7dfbb5a00d6a52758c0c7e97cc02e43ac717d2fe
Luis Eduardo Monteiro Filho <138503672+3du4rdo007@users.noreply.github.com>
2024-05-22 20:06:05 -0300

    Update conexao.php

--- TDE - Hospital/hospital-PDO/conexao.php
+++ TDE - Hospital/hospital-PDO/conexao.php
@@ -2,17 +2,45 @@
 class Conexao {
     private static $instance;
 
-    public static function getConn() {
-        if (!isset(self::$instance)) {
-            try {
-                self::$instance = new \PDO('pgsql:host=localhost;port=5432;dbname=Hospital;', 'postgres', 'postgres');
-                self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            } catch (PDOException $e) {
-                echo 'Connection failed: ' . $e->getMessage();
-            }
-        }
+    public static function getConn(){
 
+        if (!isset(self::$instance)){
+            self::$instance = new \PDO('pgsql:host=localhost;port=5432;dbname=Hospital;','postgres', 'postgres');
+        }
+    
         return self::$instance;
     }
+}
+
+class DBController {
+	private $host = "localhost";
+	private $user = "postgres";
+	private $password = "postgres";
+	private $database = "Hospital";
+	private $conn;
+	
+	function __construct() {
+		$this->conn = $this->connectDB();
+	}
+	
+	function connectDB() {
+		$conn = mysqli_connect($this->host,$this->user,$this->password,$this->database);
+		return $conn;
+	}
+	
+	function runQuery($query) {
+		$result = mysqli_query($this->conn,$query);
+		while($row=mysqli_fetch_assoc($result)) {
+			$resultset[] = $row;
+		}		
+		if(!empty($resultset))
+			return $resultset;
+	}
+	
+	function numRows($query) {
+		$result  = mysqli_query($this->conn,$query);
+		$rowcount = mysqli_num_rows($result);
+		return $rowcount;	
+	}
 }
 ?>
