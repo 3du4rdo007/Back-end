e7a246ae2844a5560ca57b16699544094cc41ffe
Luis Eduardo Monteiro Filho <138503672+3du4rdo007@users.noreply.github.com>
2024-05-22 23:42:48 -0300

    Add files via upload

---
+++ TDE - Hospital/Back/consulta.php
@@ -0,0 +1,33 @@
+<?php
+class Consulta {
+    private $ConsultaID;
+    private $Paciente;
+    private $Medico;
+    private $DataConsulta;
+
+    public function getCONSULTAID(){
+        return $this->ConsultaID;
+    }
+    public function setCONSULTAID($ConsultaID){
+        $this->ConsultaID = $ConsultaID;
+    }
+    public function getPACIENTE(){
+        return $this->Paciente;
+    }
+    public function setPACIENTE($Paciente){
+        $this->Paciente = $Paciente;
+    }
+    public function getMEDICO(){
+        return $this->Medico;
+    }
+    public function setMEDICO($Medico){
+        $this->Medico = $Medico;
+    }
+    public function getDATACONSULTA(){
+        return $this->DataConsulta;
+    }
+    public function setDATACONSULTA($DataConsulta){
+        $this->DataConsulta = $DataConsulta;
+    }
+}
+?>
---
+++ TDE - Hospital/Back/medico.php
@@ -0,0 +1,34 @@
+<?php
+
+class Medico{
+    private $MedicoID;
+    private $Nome;
+    private $Especialidade;
+    private $CRM;
+
+    public function getMEDICOID(){
+        return $this->MedicoID;
+    }
+    public function setMEDICOID($MedicoID){
+        $this->MedicoID = $MedicoID;
+    }
+    public function getNOME(){
+        return $this->Nome;
+    }
+    public function setNOME($Nome){
+        $this->Nome = $Nome;
+    }
+    public function getESPECIALIDADE(){
+        return $this->Especialidade;
+    }
+    public function setESPECIALIDADE($Especialidade){
+        $this->Especialidade = $Especialidade;
+    }
+    public function getCRM(){
+        return $this->CRM;
+    }
+    public function setCRM($CRM){
+        $this->CRM = $CRM;
+    }
+}
+?>
