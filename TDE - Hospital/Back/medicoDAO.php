7f75f94c68e38a35cdfd88b14869096f66401232
Luis Eduardo Monteiro Filho <138503672+3du4rdo007@users.noreply.github.com>
2024-05-28 12:24:22 -0300

    Update medicoDAO.php

--- TDE - Hospital/Back/medicoDAO.php
+++ TDE - Hospital/Back/medicoDAO.php
@@ -10,19 +10,20 @@ class MedicoDAO{
         $stmt->execute();
     }
 
-    public function read(){
-        $sql = 'SELECT * FROM Medico';
-
-        $stmt = Conexao::getConn()->prepare($sql);
-        $stmt->execute();
-
-        if($stmt->rowCount() > 0){
-            $resultado = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-            return $resultado;
-        }else{
-            return [];
+    public function read() {
+            $sql = 'SELECT * FROM Medico';
+            $stmt = Conexao::getConn()->prepare($sql);
+            $stmt->execute();
+    
+            if ($stmt->rowCount() > 0) {
+                return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+            } else {
+                return [];
+            }
         }
-    }
+    
+    
+    
 
     public function update(Medico $Medico) {
         $sql = 'UPDATE Medico SET Nome = ?, Especialidade = ?, CRM = ?  WHERE MedicoID = ?';
